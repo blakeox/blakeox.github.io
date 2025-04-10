513f0e2b2063f0af34943ed241b2210816ab9780
blakeox <blakeox@users.noreply.github.com>
2025-04-10 03:00:24 +0000

    deploy: 76e8718636514fb507197e9cd3bb10a11fb50a46

---
+++ assets/js/breadcrumb.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const breadcrumbContainer = document.querySelector('.breadcrumb ol');
+  if (!breadcrumbContainer) return;
+
+  const pathSegments = window.location.pathname.split('/').filter(Boolean);
+  let breadcrumbHTML = `<li><a href='/'>Home</a></li>`;
+
+  pathSegments.forEach((segment, index) => {
+    const isLast = index === pathSegments.length - 1;
+    const url = `/${pathSegments.slice(0, index + 1).join('/')}/`;
+    breadcrumbHTML += isLast
+      ? `<li aria-current='page'>${decodeURIComponent(segment.replace(/-/g, ' '))}</li>`
+      : `<li><a href='${url}'>${decodeURIComponent(segment.replace(/-/g, ' '))}</a></li>`;
+  });
+
+  breadcrumbContainer.innerHTML = breadcrumbHTML;
+
+  const breadcrumbLinks = document.querySelectorAll('.breadcrumb a');
+  breadcrumbLinks.forEach((link) => {
+    const isActive = link.href === window.location.href;
+    if (isActive) {
+      link.setAttribute('aria-current', 'page');
+    }
+  });
+});
