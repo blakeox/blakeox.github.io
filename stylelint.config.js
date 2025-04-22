81863767329e3832b880b542b8ba566d1696d378
blakeox <blakeopowell@outlook.com>
2025-04-21 22:29:22 -0400

    chore: update stylelint configuration and dependencies
    - Updated stylelint configuration to extend 'stylelint-config-standard' and 'stylelint-config-recommended-scss'.
    - Added custom syntax for SCSS and specified plugins.
    - Modified rules to allow SCSS-specific features and relaxed some restrictions.
    - Updated package.json to include new stylelint dependencies and versions.
    - Updated yarn.lock to reflect changes in dependencies and their versions.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  extends: 'stylelint-config-standard-scss',
+  // Use SCSS parser
+  customSyntax: 'postcss-scss',
+  plugins: ['stylelint-scss'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-scss'
+  ],
+  ignoreFiles: ['assets/css/main.scss'],
   rules: {
+    // Allow SCSS variables and function values
+    'declaration-property-value-no-unknown': null,
     'at-rule-no-unknown': [
       true,
       {
@@ -10,13 +19,31 @@ module.exports = {
     'block-no-empty': true,
     'color-no-invalid-hex': true,
     'declaration-block-no-duplicate-properties': true,
-    'declaration-block-trailing-semicolon': 'always',
-    'no-duplicate-selectors': true,
+    'no-duplicate-selectors': null,
     'selector-pseudo-class-no-unknown': [
       true,
       {
         ignorePseudoClasses: ['global'],
       },
     ],
+    // Allow projects to skip empty-line rules
+    'declaration-empty-line-before': null,
+    'at-rule-empty-line-before': null,
+    // Allow any hex length
+    'color-hex-length': null,
+    'no-descending-specificity': null,
+    'scss/no-global-function-names': null,
+    'rule-empty-line-before': null,
+    'scss/at-extend-no-missing-placeholder': null,
+    'color-function-notation': null,
+    'alpha-value-notation': null,
+    'selector-class-pattern': null,
+    'keyframes-name-pattern': null,
+    'value-keyword-case': null,
+    'media-feature-range-notation': null,
+    // Allow @import without url() wrapping
+    'import-notation': null,
+    // Disable alias notation errors for color functions
+    'color-function-alias-notation': null
   },
 };
